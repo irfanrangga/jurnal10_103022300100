9689561ccb34220917333db4f7e6795c2c63a7f0
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-29 07:38:47 +0700

    menambahkan API Movies

---
+++ Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace jurnal10_103022300100
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public List<string> stars { get; set; }
+        public string description { get; set; }
+
+        public Movie(string title, string director, List<string> stars, string description)
+        {
+            this.title = title;
+            this.director = director;
+            this.stars = stars;
+            this.description = description;
+        }
+    }
+}
